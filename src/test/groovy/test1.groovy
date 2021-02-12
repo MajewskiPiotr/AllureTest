caaba9047eb6ceea0be8a43c66b9808b40e312b5
Piotr Majewski <piotr.majewski@uniteam.pl>
2021-02-12 10:41:43 +0100

    first commit

---
+++ src/test/groovy/test1.groovy
@@ -0,0 +1,21 @@
+import org.junit.Test
+
+class test1 {
+
+    @org.junit.jupiter.api.Test
+    void testAluure1() {
+        int pierwszy = 1;
+        int drugi = 2
+
+        assert true
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void testAluure2() {
+        int pierwszy = 1;
+        int drugi = 2
+
+        assert false
+    }
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+    @Test
+    void allureTest2(){
+        assert true;
+    }
+}
